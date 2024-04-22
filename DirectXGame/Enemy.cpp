584fcdad2ebd132b82896ba3de8a60ba80268270
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2024-04-22 15:14:16 +0900

    敵の行動パターンを作ることができた

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -1,28 +1,57 @@
 #include "Enemy.h"
 
-void Enemy::Initialize(Model* model, const Vector3& position, const Vector3& velocity) { 
-	//nullcheak
+void Enemy::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+	// nullcheak
 	assert(model);
 	//
 	textureHandle_ = TextureManager::Load("cube/cube.jpg");
-	//3Dモデル
+	// 3Dモデル
 	model_ = model;
 	/// 3.ワールド変換の初期化
 	worldTransform_.Initialize();
 	// 引数で受け取った初期座標をセットする
 	worldTransform_.translation_ = position;
 	velocity_ = velocity;
-	worldTransform_.scale_ = {3,3, 3};
+	worldTransform_.scale_ = {3, 3, 3};
 }
 
 void Enemy::Update() {
-	// 座標を移動させる(１フレーム分の移動量を足しこむ)
-	worldTransform_.translation_ += velocity_;
+
+	/// フェーズによって行動を変化させる
+	switch (phase_) {
+	case Enemy::Phase::Approach:
+	default:
+		ApproachAction({0,0,-1});
+		break;
+	case Enemy::Phase::Leave:
+		LeaveAction({1,1,-1});
+		break;
+	}
+
 	// 行列を更新
 	worldTransform_.UpdateMatrix();
-
 }
 
-void Enemy::Draw(const ViewProjection& viewProjection) { 
-	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+void Enemy::Draw(const ViewProjection& viewProjection) { model_->Draw(worldTransform_, viewProjection, textureHandle_); }
+
+void Enemy::ApproachAction(const Vector3& velocity) {
+	
+	//approachの速度に変更
+	velocity_ = velocity;
+	
+	// 座標を移動させる(１フレーム分の移動量を足しこむ)
+	worldTransform_.translation_ += velocity_;
+
+	// 基底の位置に到達したら離脱
+	if (worldTransform_.translation_.z < 50.0f) {
+		phase_ = Phase::Leave;
+	}
+
+}
+void Enemy::LeaveAction(const Vector3& velocity) {
+	velocity_ = velocity;
+	// 座標を移動させる(１フレーム分の移動量を足しこむ)
+	worldTransform_.translation_ += velocity_;
+
+
 }
--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -11,6 +11,14 @@
 /// </summary>
 class Enemy {
 private:
+	/// <summary>
+	/// フェーズ
+	/// </summary>
+	enum class Phase {
+		Approach,	//接近する
+		Leave		//離脱する
+	};
+
 	// ワールド変換データ
 	WorldTransform worldTransform_;
 	// モデル
@@ -20,13 +28,25 @@ private:
 	// 速度
 	Vector3 velocity_;
 
+	//フェーズ
+	Phase phase_ = Phase::Approach;
+
 public:
 	void Initialize(Model* model, const Vector3& position,const Vector3& velocity);
 
 	void Update();
 
 	void Draw(const ViewProjection& viewProjection);
-
+	/// <summary>
+	/// Approachフェーズの処理
+	/// </summary>
+	/// <param name="Velocity">Approachフェーズの速度</param>
+	void ApproachAction(const Vector3& velocity); 
+	/// <summary>
+	/// Leaveフェーズの処理
+	/// </summary>
+	/// <param name="Velocity">Leaveフェーズの速度</param>
+	void LeaveAction(const Vector3& velocity); 
 	public:
 
 };
